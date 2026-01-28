40d9eeb66ad4a72822d19c4c56eb3073a84ebb1a
karnatisriram <karnatisriram2006@gmail.com>
2026-01-28 17:26:02 +0000

    added small texr with my name created by Sriram at the bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,13 @@ export default function RootLayout({
           <div className="relative flex min-h-screen w-full flex-col">
             <Header />
             <main className="flex-1">{children}</main>
+            <footer className="py-6 md:px-8 md:py-0">
+                <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
+                    <p className="text-center text-sm leading-loose text-muted-foreground">
+                    Created by Sriram.
+                    </p>
+                </div>
+            </footer>
           </div>
           <Toaster />
         </Providers>
